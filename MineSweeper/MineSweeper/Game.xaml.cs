a38eda351c264deb27fad9386f3bb668aa205145
Lars Smeby <lars@smebyfx.no>
2014-09-21 15:00:30 +0200

    ...

--- MineSweeper/MineSweeper/Game.xaml.cs
+++ MineSweeper/MineSweeper/Game.xaml.cs
@@ -2,16 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
-using Xamarin.Forms;
+using System.Threading.Tasks;
 
 namespace MineSweeper
 {
-    public class App
+    public partial class Game
     {
-        public static Page GetMainPage()
+        public Game()
         {
-            return new Game();
+            InitializeComponent();
         }
     }
 }
